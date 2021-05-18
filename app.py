9a89dceef2b59ed008cf49d97280fa53f20a93cd
alexsandercaproni <alexsander.caproni@neoenergia.com>
2021-05-17 21:18:49 -0300

    Criando Athena

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ from caproni_infrastructure.glue_catalog.stack import GlueCatalogStack
 app = core.App()
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
+athena = AthenaStack(app)
 app.synth()
