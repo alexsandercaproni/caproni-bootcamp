48546fe75b972f6449911db231a0551578c159a5
alexsandercaproni <alexsander.caproni@neoenergia.com>
2021-05-17 22:14:43 -0300

    Cria stack Kinesis

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from aws_cdk import core
 from caproni_infrastructure.data_lake.stack import DataLakeStack
 from caproni_infrastructure.glue_catalog.stack import GlueCatalogStack
 from caproni_infrastructure.athena.stack import AthenaStack
+from caproni_infrastructure.kinesis.stack import KinesisStack
 
-
+# Inicia o aplicativo
 app = core.App()
+
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket=data_lake.data_lake_raw_bucket, processed_data_lake_bucket=data_lake.data_lake_processed_bucket)
 athena = AthenaStack(app)
+kineses = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
+
+# Cria o CloudFormation
 app.synth()
