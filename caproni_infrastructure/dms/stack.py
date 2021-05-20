8b4751b767d9221180b7a054dcd81af112f83a4f
alexsandercaproni <alexsander.caproni@neoenergia.com>
2021-05-20 00:01:41 -0300

    ajustes na criacao do dms

--- caproni_infrastructure/dms/stack.py
+++ caproni_infrastructure/dms/stack.py
@@ -5,21 +5,22 @@ from aws_cdk import (
     aws_ec2 as ec2,
 )
 
+import json
+
+from caproni_infrastructure import Environment
 from common_stack import CommonStack
 from caproni_infrastructure.data_lake.base import BaseDataLakeBucket
-from caproni_infrastructure import active_environment
-
-import json
 
 
 class RawDMSRole(iam.Role):
     def __init__(
         self,
         scope: core.Construct,
+        deploy_env: Environment,
         data_lake_raw_bucket: BaseDataLakeBucket,
         **kwargs,
     ) -> None:
-        self.deploy_env = scope.deploy_env
+        self.deploy_env = deploy_env
         self.data_lake_raw_bucket = data_lake_raw_bucket
         super().__init__(
             scope,
@@ -64,12 +65,11 @@ class OrdersDMS(dms.CfnReplicationTask):
     ) -> None:
         self.data_lake_raw_bucket = data_lake_raw_bucket
         self.common_stack = common_stack
-        self.deploy_env = scope.deploy_env
         self.rds_endpoint = dms.CfnEndpoint(
             scope,
-            f"dms-{self.deploy_env.value}-orders-rds-endpoint",
+            f"dms-{self.common_stack.deploy_env.value}-orders-rds-endpoint",
             endpoint_type="source",
-            endpoint_identifier=f"dms-source-{self.deploy_env.value}-orders-rds-endpoint",
+            endpoint_identifier=f"dms-source-{self.common_stack.deploy_env.value}-orders-rds-endpoint",
             engine_name="postgres",
             password=core.CfnDynamicReference(
                 core.CfnDynamicReferenceService.SECRETS_MANAGER,
@@ -90,11 +90,11 @@ class OrdersDMS(dms.CfnReplicationTask):
 
         self.s3_endpoint = dms.CfnEndpoint(
             scope,
-            f"dms-{self.deploy_env.value}-orders-s3-endpoint",
+            f"dms-{self.common_stack.deploy_env.value}-orders-s3-endpoint",
             endpoint_type="target",
             engine_name="s3",
-            endpoint_identifier=f"dms-target-{self.deploy_env.value}-orders-s3-endpoint",
-            extra_connection_attributes="DataFormat=parquet;maxFileSize=131072;timestampColumnName=extracted_at;includeOpForFullLoad=true;cdcMaxBatchInterval=120",
+            endpoint_identifier=f"dms-target-{self.common_stack.deploy_env.value}-orders-s3-endpoint",
+            extra_connection_attributes="DataFormat=parquet;maxFileSize=131072;timestampColumnName=extracted_at;includeOpForFullLoad=true;cdcInsertsAndUpdates=true",
             s3_settings=dms.CfnEndpoint.S3SettingsProperty(
                 bucket_name=self.data_lake_raw_bucket.bucket_name,
                 bucket_folder="orders",
@@ -102,37 +102,37 @@ class OrdersDMS(dms.CfnReplicationTask):
                 csv_delimiter=",",
                 csv_row_delimiter="\n",
                 service_access_role_arn=RawDMSRole(
-                    scope, self.data_lake_raw_bucket
+                    scope, self.common_stack.deploy_env, self.data_lake_raw_bucket
                 ).role_arn,
             ),
         )
 
         self.dms_sg = ec2.SecurityGroup(
             scope,
-            f"dms-{self.deploy_env.value}-sg",
+            f"dms-{self.common_stack.deploy_env.value}-sg",
             vpc=self.common_stack.custom_vpc,
-            security_group_name=f"dms-{self.deploy_env.value}-sg",
+            security_group_name=f"dms-{self.common_stack.deploy_env.value}-sg",
         )
 
         self.dms_subnet_group = dms.CfnReplicationSubnetGroup(
             scope,
-            f"dms-{self.deploy_env.value}-replication-subnet",
+            f"dms-{self.common_stack.deploy_env.value}-replication-subnet",
             replication_subnet_group_description="dms replication instance subnet group",
             subnet_ids=[
                 subnet.subnet_id
                 for subnet in self.common_stack.custom_vpc.private_subnets
             ],
-            replication_subnet_group_identifier=f"dms-{self.deploy_env.value}-replication-subnet",
+            replication_subnet_group_identifier=f"dms-{self.common_stack.deploy_env.value}-replication-subnet",
         )
 
         self.instance = dms.CfnReplicationInstance(
             scope,
-            f"dms-replication-instance-{self.deploy_env.value}",
+            f"dms-replication-instance-{self.common_stack.deploy_env.value}",
             allocated_storage=100,
             publicly_accessible=False,
-            engine_version="3.4.4",
+            engine_version="3.3.3",
             replication_instance_class="dms.t2.small",
-            replication_instance_identifier=f"dms-{self.deploy_env.value}-replication-instance",
+            replication_instance_identifier=f"dms-{self.common_stack.deploy_env.value}-replication-instance",
             vpc_security_group_ids=[self.dms_sg.security_group_id],
             replication_subnet_group_identifier=self.dms_subnet_group.replication_subnet_group_identifier,
         )
@@ -142,9 +142,9 @@ class OrdersDMS(dms.CfnReplicationTask):
 
         super().__init__(
             scope,
-            f"{self.deploy_env.value}-dms-task-orders-rds",
+            f"{self.common_stack.deploy_env.value}-dms-task-orders-rds",
             migration_type="full-load-and-cdc",
-            replication_task_identifier=f"{self.deploy_env.value}-dms-task-orders-rds",
+            replication_task_identifier=f"{self.common_stack.deploy_env.value}-dms-task-orders-rds",
             replication_instance_arn=self.instance.ref,
             source_endpoint_arn=self.rds_endpoint.ref,
             target_endpoint_arn=self.s3_endpoint.ref,
@@ -176,7 +176,7 @@ class DmsStack(core.Stack):
         data_lake_raw_bucket: BaseDataLakeBucket,
         **kwargs,
     ) -> None:
-        self.deploy_env = active_environment
+        self.deploy_env = common_stack.deploy_env
         self.data_lake_raw_bucket = data_lake_raw_bucket
         super().__init__(scope, id=f"{self.deploy_env.value}-dms-stack", **kwargs)
 
